75343ea1b8eacbda0d81a2aaaeb093d693872e32
Adam Sebzda <asebzda@gmail.com>
2020-10-08 23:13:20 -0400

    Added test cases

--- test/serex_test.exs
+++ test/serex_test.exs
@@ -1,8 +1,39 @@
 defmodule SerexTest do
   use ExUnit.Case
-  doctest Serex
+  doctest Serex.Lexer
+  doctest Serex.Matcher
 
-  test "greets the world" do
-    assert Serex.hello() == :world
+  test "Regex matching normal characters" do
+    assert Serex.Matcher.match("a", "abc") == true
+    assert Serex.Matcher.match("bc", "abc") == true
+    assert Serex.Matcher.match("abc", "abc") == true
+    assert Serex.Matcher.match("d", "abc") == false
+  end
+
+  test "Regex matching beginning of line" do
+    assert Serex.Matcher.match("^a", "abc") == true
+    assert Serex.Matcher.match("^abc", "abc") == true
+    assert Serex.Matcher.match("^b", "abc") == false
+  end
+
+  test "Regex matching end of line" do
+    assert Serex.Matcher.match("c$", "abc") == true
+    assert Serex.Matcher.match("abc$", "abc") == true
+    assert Serex.Matcher.match("b$", "abc") == false
+  end
+
+  test "Regex matching wildcards" do
+    assert Serex.Matcher.match(".bc", "abc") == true
+    assert Serex.Matcher.match("a.c", "abc") == true
+    assert Serex.Matcher.match("b.", "abc") == true
+    assert Serex.Matcher.match(".ac", "abc") == false
+  end
+
+  test "Regex matching zero or more" do
+    assert Serex.Matcher.match("a*bc", "abc") == true
+    assert Serex.Matcher.match("a*bc", "aaaaabc") == true
+    assert Serex.Matcher.match("a*bc", "bc") == true
+    assert Serex.Matcher.match("a*bc", "aaab") == false
+    assert Serex.Matcher.match("a*bc", "bbbbbbccc") == true
   end
 end
